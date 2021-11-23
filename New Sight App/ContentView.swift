27ea465cd677b16934748a6b5596926d4173744b
Ankita-Rawat <go2ankitarawat@gmail.com>
2021-11-23 20:20:25 +0530

    Initial Commit

---
+++ New Sight App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  New Sight App
+//
+//  Created by Bharat Singh on 23/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ New Sight App/New_Sight_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Sight_AppApp.swift
+//  New Sight App
+//
+//  Created by Bharat Singh on 23/11/21.
+//
+
+import SwiftUI
+
+@main
+struct New_Sight_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
